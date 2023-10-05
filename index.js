28d7866d779999080286ab653185f352ce01c690
MaiT-027 <mytmicekorea@naver.com>
2023-10-05 17:23:31 +0900

    cors, bodyParser 추가

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const body_parser_1 = __importDefault(require("body-parser"));
 const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
+app.use(body_parser_1.default.json());
 app.get("/", (_req, res) => {
     res.json({ response: "OK" });
 });
