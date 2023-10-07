566666808b7622eee56b275a9bb0551228358857
MaiT-027 <mytmicekorea@naver.com>
2023-10-07 09:42:23 +0900

    홈 디렉토리 반환값 변경

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
 app.use(body_parser_1.default.json());
 app.get("/", (_req, res) => {
-    res.json({ response: "OK" });
+    res.json({ result: "OK" });
 });
 app.listen(3000, () => {
     console.log("Server started on Port 3000");
